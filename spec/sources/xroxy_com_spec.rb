5256ec8d9540260c0512ed818634c94d93f93d9e
Francis Chong <francis@ignition.hk>
2013-12-13 17:35:26 +0800

    add country to xpoxy proxy list, set proper default page number

--- spec/sources/xroxy_com_spec.rb
+++ spec/sources/xroxy_com_spec.rb
@@ -1,10 +1,9 @@
 require 'proxy_list/sources/xroxy_com'
-require 'pry'
 
 describe ProxyList::Sources::XroxyCom do
   subject { Object.new.extend(ProxyList::Sources::XroxyCom) }
 
-  context "#proxy" do
+  context "#list" do
     it "should find proxy list" do
       proxies = subject.list(page_count: 1)
       proxies.should_not be_nil
